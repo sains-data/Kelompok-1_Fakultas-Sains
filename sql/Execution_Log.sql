5804a43f8a491dc267be82fc6c4c20f7e051d274
Vania Claresta <clarestavania11@gmail.com>
2025-12-07 19:38:46 +0700

    Create Execution_Log.sql

---
+++ sql/Execution_Log.sql
@@ -0,0 +1,24 @@
+EXEC dbo.usp_Load_Dim_Prodi;
+
+-- 2. Load Referensi Independen
+EXEC dbo.usp_Load_Dim_Jurnal;
+
+-- 3. Load Dimensi Anak (Butuh Prodi)
+EXEC dbo.usp_Load_Dim_MataKuliah;
+EXEC dbo.usp_Load_Dim_Dosen;
+EXEC dbo.usp_Load_Dim_Mahasiswa;  -- (Yang sudah Anda buat tadi)
+
+SELECT * FROM dbo.Dim_Prodi;
+SELECT * FROM dbo.Dim_Dosen;
+SELECT * FROM dbo.Dim_MataKuliah;
+SELECT * FROM dbo.Dim_Mahasiswa;
+
+-- Load Fakta Akademik
+EXEC dbo.usp_Load_Fakta_Akademik;
+
+-- Load Fakta Publikasi
+EXEC dbo.usp_Load_Fakta_Publikasi;
+
+-- Cek Hasil Akhir
+SELECT COUNT(*) AS Total_Fakta_Akademik FROM dbo.Fakta_Akademik;
+SELECT COUNT(*) AS Total_Fakta_Publikasi FROM dbo.Fakta_Publikasi;
